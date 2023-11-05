3ea694e5daa986d07666e43b1a28cb4b9430e8fd
nachatra-sharma <nakshusharm0@gmail.com>
2023-11-05 17:01:12 -0500

    Introduction to programming

---
+++ 1. Day - 1 | C++ & DSA/2. DataTypes.cpp
@@ -0,0 +1,21 @@
+// Data Type
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a; // Int == Integer
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Your entered number is " << a << endl;
+    float f; // Float == Decimals
+    cout << "Enter value of PI: ";
+    cin >> f;
+    cout << "Value of PI is: " << f << endl;
+    double d = 2.12345678; // Double == Big Decimals
+    cout << "Value of double is: " << d << endl;
+    char c = 'a'; // Char == Character
+    cout << "Entered character value is: " << c << endl;
+    bool b = true;
+    cout << "Your age is 18 that is: " << b << endl;
+}
+
